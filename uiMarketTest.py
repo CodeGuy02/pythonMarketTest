b4c4926990ab213356f96279cede2b21c909ba35
codeguy02 <codeguy02@example.com>
2021-10-11 08:38:12 -0400

    UI additions

--- uiMarketTest.py
+++ uiMarketTest.py
@@ -44,3 +44,63 @@ def launch_script(self):
         self.BalancesAndPositions()
 
         self.priceHistory()
+
+def displaySystemStatus(self):
+        self.AccountDetailsText.append('                                    Test Version 0.1                          ')
+        self.AccountDetailsText.append('_____________________________________________________________________________________________')
+        self.AccountDetailsText.append('Machine:    ' + str(platform.machine()))
+        self.AccountDetailsText.append('System:     ' + str(platform.platform()))
+        self.AccountDetailsText.append('_____________________________________________________________________________________________')
+
+
+def updateUIwork(self,val):
+        val = str(val)
+        #self.ActivityText.append(val)
+
+def MarketTableDefaultValues(self, default_string):
+        rows = 7
+        row = 0
+        while row < rows:
+            self.MarketTable.setItem(row, 0, QtGui.QTableWidgetItem(str(default_string)))
+            row = row + 1
+
+def updateUItestCompletion(self, percent):
+        self.BackTestDataCompletionProgressBar.setValue(percent)
+
+def updateUIchartValuesCompletion(self, key):
+        self.ActivityText.append(str(key))
+
+def updateUItestStrategyComboBox(self):
+        comboName = str(self.TestStrategyComboBox.currentText())
+        row = self.lastStrategyAvailableCellClicked
+        self.StrategyAvailableTable.setItem(row, 0, QtGui.QTableWidgetItem(str(comboName)))
+
+def updateUIstockBought(self, buy_notice):
+        self.ActivityText.append(buy_notice)
+
+def updateUIstockSold(self, sold_notice):
+        self.ActivityText.append(sold_notice)
+
+
+def strategy_available_cell_clicked(self, row, column):
+        self.lastStrategyAvailableCellClicked = row
+        self.updateUIstartingStrategyValues()
+
+def marketCellClicked(self, row, column):
+        self.lastMarketSelected = row
+
+def updateUImarketBackTestComboBox(self):
+        comboName = str(self.MarketBackTestComboBox.currentText())
+        row = self.lastStrategyAvailableCellClicked
+        self.BackTestingTable.setItem(row, 0, QtGui.QTableWidgetItem(str(comboName)))
+
+def updateUIcandlestickComboBox(self):
+        comboName = str(self.CandlestickComboBox.currentText())
+        row = self.lastStrategyAvailableCellClicked
+        self.BackTestingTable.setItem(row, 1, QtGui.QTableWidgetItem(str(comboName)))
+
+def updateUIdaysComboBox(self):
+        comboName = str(self.DaysComboBox.currentText())
+        row = self.lastStrategyAvailableCellClicked
+        self.BackTestingTable.setItem(row, 2, QtGui.QTableWidgetItem(str(comboName)))
+
