8ed5edbf648d1eb71d418a55f0bf7ce85bc6bdbb
codeguy02 <codeguy02@example.com>
2021-10-28 23:25:17 -0400

    ui Market Test - generating graphs

--- uiMarketTest.py
+++ uiMarketTest.py
@@ -240,3 +240,56 @@ def updateUIdaysComboBox(self):
         row = self.lastStrategyAvailableCellClicked
         self.BackTestingTable.setItem(row, 2, QtGui.QTableWidgetItem(str(comboName)))
 
+def generateGraphs(self):
+        print('    ------ Generating Graphs --------------')
+        if self.marketName == 'TD Ameritrade':
+            plot_data = {}
+            graphs_ohlc_data = self.TDAmeritrade_data
+            for currencyPair, chartData in graphs_ohlc_data.iteritems():
+                #print('currencyPair to graph = ' + str(currencyPair))
+                fig = plt.figure()
+
+                #fig.patch.set_facecolor('#260031') # Deep lavender ?
+                fig.patch.set_facecolor('#000034')
+
+
+                pltOHLC = fig.add_subplot(311) # Obtain subplot from fig
+
+                pltOHLC.grid(True)
+                pltOHLC.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode='expand', borderaxespad=0.)
+                # new_market = str(goodVolume[1])
+                new_market = str(currencyPair)
+                new_ohlc = graphs_ohlc_data[new_market]
+
+                #print('creating new plot for: ' + new_market)
+                # ax = Axes.minorticks_on(Axes)
+                if (self.days == '1 day') or (self.days == '2 days') or (self.days == '1 week'):
+                    #print('_____using smaller bars...')
+                    #candlestick_ohlc(pltOHLC, new_ohlc, width=0.0002, colorup='#baffc9', colordown='#ffb3ba')
+                    candlestick_ohlc(pltOHLC, new_ohlc, width=0.0002, colorup='#aa2d7d', colordown='#611d6d')
+                else:
+                    print('_____using CHUNKY bars...')
+                    candlestick_ohlc(pltOHLC, new_ohlc, width=0.2, colorup='#aa2d7d', colordown='#611d6d')
+
+                for label in pltOHLC.xaxis.get_ticklabels():
+                    label.set_rotation(0)
+
+                # pltOHLC.xaxis.set_major_locator(mdates.DayLocator())
+                pltOHLC.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+                # pltOHLC.set_xticks(ohlc[0][0], ohlc[(bar_count - 1)][0])
+
+                #description = self.descriptionList[new_market]
+                #pltOHLC.set_title(new_market + ' - ' + str(self.marketName) + ' - ' + str(description))
+                pltOHLC.set_title(new_market + ' - ' + str(self.marketName))
+                pltOHLC.set_xlabel('Date')
+                pltOHLC.set_ylabel('Price')
+
+                # print(graphs_ohlc_data[str(goodVolume[0])])
+                bar_count = len(graphs_ohlc_data[new_market])
+                #print('bar count for graph test = ' + str(bar_count))
+
+                xmin = graphs_ohlc_data[new_market][0][0]
+                xmax = graphs_ohlc_data[new_market][(bar_count - 1)][0]
+                # xmin = 0
+                # xmax = bar_count
+                # append_day = time, stock_open, stock_high, stock_low, stock_close, stock_volume
